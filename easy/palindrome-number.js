9c4f27a345c7a278d83f251912e3ee290efa78a2
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-02-17 20:26:33 -0600

    palindrome number refactor

--- easy/palindrome-number.js
+++ easy/palindrome-number.js
@@ -3,17 +3,9 @@
  * @return {boolean}
  */
 const isPalindrome = function(x) {
-  let stringNum = x.toString()
-  let left
-  let right
-  if(stringNum.length % 2 !== 0) {    
-    left = stringNum.slice(0, Math.floor(stringNum.length/2))
-    right = stringNum.slice(Math.floor(stringNum.length/2) + 1)
-  } else {
-    left = stringNum.slice(0, stringNum.length/2)
-    right = stringNum.slice(stringNum.length/2)
-  }   
-  return right.split("").reverse().join("") === left
+  let xToString = x.toString()
+  let stringNum = xToString.split("").reverse().join("")  
+  return stringNum === xToString
 }
 
 isPalindrome(-1221)
